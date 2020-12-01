50ce62a56cec1e9be6eb0ab10c3781837efce622
zhangtingfeng-2000 <2229596898@qq.com>
2020-12-01 12:51:49 +0800

    注释

--- Algorithms/KMP.cpp
+++ Algorithms/KMP.cpp
@@ -8,8 +8,21 @@ void kmp_pre(char x[], int m, int next[]) {
 	}
 }
 
+void preKMP(char x[], int m, int KmpNext[]) {
+	int i, j;
+	j = KmpNext[0] = -1;
+	i = 0;
+	while (i < m) {
+		while(-1 != j && x[i] != x[j]) j = KmpNext[j];
+		if (x[++i] == x[++j]) KmpNext[i] = KmpNext[j];
+		else KmpNext[i] = j;
+	}
+}
+
+int KmpNext[10010];
 int next[10010];
 
+// 返回模式串的出现次数
 int KMP_Count(char x[], int m, char y[], int n) {
 	int i, j;
 	int ans = 0;
